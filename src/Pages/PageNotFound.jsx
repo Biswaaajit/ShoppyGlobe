31ba356922d6fc509c416b9898ff7e9802975e6c
Biswajeet Barik <biswajeetbarik3936@gmail.com>
2025-01-26 23:10:50 +0530

    done some styling

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -3,12 +3,12 @@ import { useNavigate } from "react-router-dom";
 function PageNotFound() {
   const navigate = useNavigate();
   return (
-    <div className="h-screen flex flex-col justify-center items-center">
-      <p>Error 404 </p>
+    <div className="h-screen flex flex-col justify-center items-center gap-3">
+      <p className="text-xl font-semibold">Error 404 </p>
       <p>Sorry the page you are looking for does not exist</p>
       <button
         onClick={() => navigate("/")}
-        className="bg-slate-200 px-6 py-1.5 rounded-lg mt-5 hover:text-white hover:bg-black"
+        className="bg-slate-200 px-6 py-1.5 rounded-lg  hover:text-white hover:bg-black"
       >
         Back to homepage
       </button>
