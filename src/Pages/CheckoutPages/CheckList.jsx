1179ab79c6f2ec4b979f842350eafa58c2aad488
Biswajeet Barik <biswajeetbarik3936@gmail.com>
2025-01-26 20:16:44 +0530

    updated styling in chekoutpage

--- src/Pages/CheckoutPages/CheckList.jsx
+++ src/Pages/CheckoutPages/CheckList.jsx
@@ -3,12 +3,12 @@ import proptype from "prop-types";
 function CheckList({ item }) {
   return (
     <div className="flex gap-2 w-full items-center justify-between py-4 border-b-2 border-slate-200">
-      <div className=" flex flex-nowrap text-sm gap-3">
+      <div className=" flex flex-wrap text-sm gap-3">
         <p>{item.title}</p>
         <p>x</p>
         <p>{item.item}</p>
       </div>
-      <p className="font-semibold">$ {item.totalPrice}</p>
+      <p className="font-semibold text-nowrap">$ {item.totalPrice}</p>
     </div>
   );
 }
